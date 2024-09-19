195e0570e16a633cd82a9e87f26442b326266fc2
riesmarahman <riesmarahmann@gmail.com>
2024-09-19 20:43:50 +0700

    Up

--- Test Suites/APITest.ts
+++ Test Suites/APITest.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>fbcb5b14-5f66-4d2c-b125-916664e93a25</testSuiteGuid>
+   <testCaseLink>
+      <guid>7b6b72bb-8a5f-4475-a284-2ab8ccf5b28d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/APITest</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
